bc6088444f7a5048080558059277c11e021bcf3d
tlaham <tristanstorm66@gmail.com>
2022-04-25 08:30:35 -0400

    First commit, added test psql involving a display as well as a login feature

---
+++ PSQLTest/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace PSQLTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;Port=5432;Database=cop4710;User Id=postgres;Password=spring99");
+            conn.Open();
+            NpgsqlCommand cmd = new NpgsqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from users";
+            NpgsqlDataReader dr = cmd.ExecuteReader();
+            if(dr.HasRows)
+            {
+                DataTable dt = new DataTable();
+                dt.Load(dr);
+                dataGridView1.DataSource = dt;
+            }
+            cmd.Dispose();
+            conn.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;Port=5432;Database=cop4710;User Id=postgres;Password=spring99");
+            conn.Open();
+            NpgsqlCommand cmd = new NpgsqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = $"select u_id from users where u_name='{textBox1.Text}' and u_pswd='{textBox2.Text}'";
+            NpgsqlDataReader dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                this.Hide();
+                MainPage mp = new MainPage();
+                mp.Show();
+            }
+            cmd.Dispose();
+            conn.Close();
+        }
+    }
+}
