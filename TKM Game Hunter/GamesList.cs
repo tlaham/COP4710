6cbf4bfc6648294e8eabdb6a663069bc0303e3d8
MattSkelton <mjskelton77@gmail.com>
2022-04-27 18:39:41 -0400

    Revert "Implemented the game editor, games can now be added and deleted, update(inprogress)"
    This reverts commit a26554182014a831fe4c111bd9568c8c18e42160.

--- TKM Game Hunter/GamesList.cs
+++ TKM Game Hunter/GamesList.cs
@@ -15,9 +15,6 @@ namespace TKM_Game_Hunter
     public partial class GamesList : Form
     {
         private NpgsqlConnection conn;
-        private NpgsqlCommand cmd;
-        private int rowindex = -1;
-        private string imgPath = string.Empty;
         public GamesList()
         {
             InitializeComponent();
@@ -32,7 +29,7 @@ namespace TKM_Game_Hunter
         private void Select()
         {
             conn.Open();
-            cmd = new NpgsqlCommand("select * from game", conn);
+            NpgsqlCommand cmd = new NpgsqlCommand("select * from game", conn);
             DataTable dt = new DataTable();
             dt.Load(cmd.ExecuteReader());
             conn.Close();
@@ -44,96 +41,8 @@ namespace TKM_Game_Hunter
         {
             if(e.RowIndex >= 0)
             {
-                rowindex = e.RowIndex;
                 txtbx_title.Text = dgv_games.Rows[e.RowIndex].Cells["game_name"].Value.ToString();
-                txtbx_genre.Text = dgv_games.Rows[e.RowIndex].Cells["genre"].Value.ToString();
-                txtbx_platform.Text = dgv_games.Rows[e.RowIndex].Cells["platform"].Value.ToString();
-                txtbx_price.Text = dgv_games.Rows[e.RowIndex].Cells["price"].Value.ToString();
-                txtbx_company.Text = dgv_games.Rows[e.RowIndex].Cells["company"].Value.ToString();
-                if (dgv_games.Rows[e.RowIndex].Cells["splashart"].Value.ToString()!=string.Empty)
-                {
-                    pbx_splash.BackgroundImage = Image.FromFile(dgv_games.Rows[e.RowIndex].Cells["splashart"].Value.ToString());
-                    pbx_splash.BackgroundImageLayout = ImageLayout.Stretch;
-                }
-                else
-                {
-                    pbx_splash.BackgroundImage = null;
-                }
-            }
-        }
-
-        private void but_insert_Click(object sender, EventArgs e)
-        {
-            rowindex = -1;
-            txtbx_title.Text = txtbx_genre.Text = txtbx_platform.Text = txtbx_company.Text = txtbx_price.Text = null;
-            pbx_splash.BackgroundImage = null;
-            imgPath = string.Empty;
-        }
-
-        private void but_update_Click(object sender, EventArgs e)
-        {
-            if(rowindex<0)
-            {
-                MessageBox.Show("Please Choose a game to update", "Alert", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-                return;
-            }
-        }
-
-        private void but_delete_Click(object sender, EventArgs e)
-        {
-            if (rowindex < 0)
-            {
-                MessageBox.Show("Please Choose a game to delete", "Alert", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-                return;
-            }
-            try
-            {
-                conn.Open();
-                int id2remove = int.Parse(dgv_games.Rows[rowindex].Cells["game_id"].Value.ToString());
-                cmd = new NpgsqlCommand($"delete from game where game_id={id2remove}", conn);
-                cmd.ExecuteNonQuery();
-                MessageBox.Show("Deleted Game Successfully");
-                rowindex = -1;
-                conn.Close();
-                Select();
-            }
-            catch (Exception ex)
-            {
-                conn.Close();
-                MessageBox.Show($"Delete Failed Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
-
-        private void but_save_Click(object sender, EventArgs e)
-        {
-           if(rowindex==-1)
-            {
-                try
-                {
-                    conn.Open();
-                    cmd = new NpgsqlCommand($"INSERT INTO Game (game_name,genre,price,company,platform,splashart) VALUES('{txtbx_title.Text}'," +
-                        $"'{txtbx_genre.Text}','{txtbx_price.Text}','{txtbx_company.Text}','{txtbx_platform.Text}','{imgPath}')", conn);
-                    cmd.ExecuteNonQuery();
-                    MessageBox.Show("Game Added Successfully");
-                    conn.Close();
-                    Select();
-                }
-                catch (Exception ex)
-                {
-                    conn.Close();
-                    MessageBox.Show($"Insertion Failed Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            }
-        }
-
-        private void but_browse_Click(object sender, EventArgs e)
-        {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "Image Files (*.jpg;*.jpeg;.*.gif;)|*.jpg;*.jpeg;.*.gif";
-            if (openFileDialog.ShowDialog() == DialogResult.OK)
-            {
-                imgPath = openFileDialog.FileName;
-                pbx_splash.BackgroundImage = new Bitmap(openFileDialog.FileName);
+                pbx_splash.BackgroundImage = Image.FromFile(dgv_games.Rows[e.RowIndex].Cells["splashart"].Value.ToString());
                 pbx_splash.BackgroundImageLayout = ImageLayout.Stretch;
             }
         }
